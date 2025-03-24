65526cfbad83b17eba5866b9c02850d33f8581b8
Hannah Choe <hanchoe@bu.edu>
2025-03-24 15:22:06 -0400

    Built site for gh-pages

--- scripts/clean_data.R
+++ scripts/clean_data.R
@@ -4,10 +4,46 @@
 
 library(tidyverse)
 
-loan_data <- read_csv(here::here("dataset", "loan_refusal.csv"))
+
+demographic_data <- read_csv(here::here("dataset", "demographic_data.csv")) 
 
 ## CLEAN the data
-loan_data_clean <- loan_data |>
-  pivot_longer(2:5, names_to = "group", values_to = "refusal_rate")
+##loan_data_clean <- loan_data |>
+  ##pivot_longer(2:5, names_to = "group", values_to = "refusal_rate")
 
-write_rds(loan_data_clean, file = here::here("dataset", "loan_refusal_clean.rds"))
+write_rds(demographic_data, file = here::here("dataset", "demographic_data.rds"))
+
+pollution_data <- read_csv(here::here("dataset", "pollution_data.csv"), show_col_types = FALSE)
+
+## CLEAN the data
+##loan_data_clean <- loan_data |>
+##pivot_longer(2:5, names_to = "group", values_to = "refusal_rate")
+
+write_rds(pollution_data, file = here::here("dataset", "pollution_data.rds"))
+
+# Merging demographic data to pollution data
+cleaned_dataset <- merge(pollution_data, demographic_data,by = "Census Tract", all.x = TRUE)
+
+# Removing Columns
+columns_to_remove_patterns <- c(
+  "Drinking Water", "Lead", "Pesticides", "Groundwater Threats", 
+  "Imp. Water Bodies", "Education", "Linguistic Isolation", 
+  "Poverty", "Unemployment", "Housing Burden"
+)
+
+# Remove the columns
+for (pattern in columns_to_remove_patterns) {
+  cleaned_dataset <- cleaned_dataset |>
+    select(-contains(pattern))
+}
+
+# Seeing how many Null values within dataset and dropping them
+colSums(is.na(cleaned_dataset)) # seems like Low birth has the largest amount of
+# missing values, should we still drop everything?
+cleaned_dataset <- cleaned_dataset |>
+  drop_na()
+
+# Writing RDS
+write_rds(cleaned_dataset, file = here::here("dataset", "cleaned_dataset.rds"))
+
+
