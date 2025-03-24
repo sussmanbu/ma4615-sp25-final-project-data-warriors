fc9774bd9f5a41982abb5cf23f69a52299ab18fe
Hannah Choe <hanchoe@bu.edu>
2025-03-24 01:54:34 -0400

    Cleaned the data: removed columns

--- scripts/clean_data.R
+++ scripts/clean_data.R
@@ -3,6 +3,7 @@
 # to separate files in order to work on different aspects of your project
 
 library(tidyverse)
+library(dplyr)
 
 
 demographic_data <- read_csv(here::here("dataset", "demographic_data.csv")) 
@@ -21,7 +22,22 @@ pollution_data <- read_csv(here::here("dataset", "pollution_data.csv"), show_col
 
 write_rds(pollution_data, file = here::here("dataset", "pollution_data.rds"))
 
+# Merging demographic data to pollution data
 cleaned_dataset <- merge(pollution_data, demographic_data,by = "Census Tract", all.x = TRUE)
-print (cleaned_dataset)
 
+# Removing Columns
+columns_to_remove_patterns <- c(
+  "Drinking Water", "Lead", "Pesticides", "Groundwater Threats", 
+  "Imp. Water Bodies", "Education", "Linguistic Isolation", 
+  "Poverty", "Unemployment", "Housing Burden"
+)
+
+# Remove the columns
+for (pattern in columns_to_remove_patterns) {
+  cleaned_dataset <- cleaned_dataset %>%
+    select(-contains(pattern))
+}
+
+# Writing RDS
 write_rds(cleaned_dataset, file = here::here("dataset", "cleaned_dataset.rds"))
+
