cb43d0029f0c369681f1dff0c14a892f8d62be9f
Hannah Choe <hanchoe@bu.edu>
2025-04-22 22:14:44 -0400

    adding dataset to shiny folder

--- scripts/create_shiny_dataset.R
+++ scripts/create_shiny_dataset.R
@@ -4,6 +4,9 @@ library(tidyverse)
 
 loan_data <- read_rds(here::here("dataset", "loan_refusal_clean.rds")) |>
   filter(group == "min")
-
-
 write_rds(loan_data, file = here::here("dataset_for_shiny", "loan_refusal_shiny.rds"))
+
+
+cleaned_data <- read_rds(here::here("dataset", "cleaned_dataset.rds"))
+write_rds(cleaned_data, file = here::here("dataset_for_shiny", "cleaned_dataset.rds"))
+
