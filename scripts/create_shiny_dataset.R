85b136149d797fcf037778cb367a341c5d0eec8b
Hannah Choe <hanchoe@bu.edu>
2025-04-22 22:49:20 -0400

    updated script for interactive data in shiny folder

--- scripts/create_shiny_dataset.R
+++ scripts/create_shiny_dataset.R
@@ -7,6 +7,12 @@ loan_data <- read_rds(here::here("dataset", "loan_refusal_clean.rds")) |>
 write_rds(loan_data, file = here::here("dataset_for_shiny", "loan_refusal_shiny.rds"))
 
 
-cleaned_data <- read_rds(here::here("dataset", "cleaned_dataset.rds"))
-write_rds(cleaned_data, file = here::here("dataset_for_shiny", "cleaned_dataset.rds"))
+cleaned_data <- read_rds(here::here("dataset", "cleaned_dataset.rds")) 
+county_demographic <- cleaned_data |>
+  select(
+    `California County`, `Pollution Burden Score`,
+    `Hispanic (%)`, `White (%)`, `African American (%)`,
+    `Asian American (%)`, `Native American (%)`, `Other/Multiple (%)`
+  )
+write_rds(county_demographic, file = here::here("dataset_for_shiny", "county_demographic.rds"))
 
